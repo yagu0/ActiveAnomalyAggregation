356814a60cf266e62b1756b097a3a2a7d12679be
Kevin Bleakley <kevinbleakley@users.noreply.github.com>
2025-07-23 15:28:10 +0200

    Update reproducible.py

--- src/acanag/reproducible.py
+++ src/acanag/reproducible.py
@@ -481,7 +481,8 @@ def ActiveAGG(X_new = None, X_old = None, X_lab = None, Y_lab = None, all_labele
                     module__input_dim=all_labeled_scores.shape[1],
                     max_epochs=20,
                     lr=0.01,
-                    verbose=0
+                    verbose=0,$
+                    callbacks=[]
                 )
                 
                 learned_model = net.fit(
@@ -598,7 +599,8 @@ def ActiveAGG(X_new = None, X_old = None, X_lab = None, Y_lab = None, all_labele
                         module__input_dim=curr_all_labeled_scores.shape[1],
                         max_epochs=20,
                         lr=0.01,
-                        verbose=0
+                        verbose=0,
+                        callbacks=[]
                     ),
                     query_strategy=margin_sampling,
                     X_training=curr_all_labeled_scores.astype(np.float32),
